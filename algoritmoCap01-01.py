8d2a7c1d517e6b61e9ac41e053e101270bb962cd
walbertcm <walbertcm@gmail.com>
2020-10-18 17:51:57 -0300

    Create algoritmoCap01-01.py
    algoritmoCap01-01

---
+++ algoritmoCap01-01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  3 11:45:38 2020
+
+@author: homeland
+"""
+
+#Comentarios em Python são iniciados pelo caracter #
+#Um caracter nunca é interpretado pela linguagem Python
