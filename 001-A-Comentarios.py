04fd5e89e62ced8268d97eee7bb81bdfbc01e6ef
WalbertCM <walbertcm@gmail.com>
2021-11-21 11:56:41 -0300

    21112021.B

--- 001-A-Comentarios.py
+++ 001-A-Comentarios.py
@@ -1,15 +1,15 @@
 
 #COMETÁRIOS EM PYTHON
 
-#Um comentário, é uma texto, informação que é iniciado com
+#Um comentário, é uma texto que é iniciado com
 #o simbolo "#"
 
 #Comentários podem ser usados ​​para explicar o código Python.
 
 #Comentários podem ser usados ​​para tornar o código mais legível.
 
-#comentários podem ser usados ​​para evitar a execução de uma parte 
+#Comentários podem ser usados ​​para evitar a execução de uma parte 
 #do codigo ao testar o algoritmo.
 
 #IMPORTANTE
-#Um COMANTÁRIO nunca é interpretado pela linguagem Python
+#Um COMENTÁRIO nunca é interpretado pela linguagem Python
