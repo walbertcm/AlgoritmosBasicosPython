671c2e6bde0855a9a19897699760442ee85b3a12
WalbertCM <walbertcm@gmail.com>
2021-11-21 12:46:18 -0300

    21.11.2021

---
+++ 004-A-Operadores.py
@@ -0,0 +1,58 @@
+#OPERADORES
+#Operadores são usados ​​para realizar operações em variáveis ​​e valores.
+
+#Operadores Python
+#Python divide os operadores nos seguintes grupos:
+
+#Operadores aritméticos
+#Operadores de atribuição
+#Operadores de comparação
+#Operadores lógicos
+#Operadores de identidade
+#Operadores de associação
+#Operadores bit a bit
+
+#############################
+#Operadores aritméticos Python
+#Operadores aritméticos são usados ​​com valores numéricos para realizar operações matemáticas comuns:
+# +	Addition	x + y	
+# -	Subtraction	x - y	
+# *	Multiplication	x * y	
+# /	Division	x / y	
+# %	Modulus	x % y	(RESTO DA DIVISÃO)
+# **	Exponentiation	x ** y	
+# //	Floor division	x // y (RESULTADO DA DIVISÃO EXATO, SEM CASAS DECIMAIS)
+
+#EXEMPLOS
+
+A = 10
+B = 20
+C = 30
+
+#####################
+##ADIÇÃO
+print(A+B)
+
+#####################
+##SUBTRAÇÃO
+print(A-B)
+
+#####################
+##ADIÇÃO
+print(A*B)
+
+#####################
+##DIVISÃO
+print(B/A)
+
+#####################
+##MODULUS (RESTO DA DIVISÃO)
+print(5%2)
+
+#####################
+##EXPONENCIAÇÃO
+print(5**2)
+
+#####################
+##FLOOR DIVISION (RESULTADO DA DIVISÃO EXATO, SEM CASAS DECIMAIS)
+print(5//2)
---
+++ 004-B-Operadores.py
@@ -0,0 +1,40 @@
+#Operadores de atribuição Python
+#Operadores de atribuição são usados ​​para atribuir valores a variáveis:
+
+#Atribuir valor (=)	x = 5	x = 5	
+#Incrementar Valor (+=)	x += 3	x = x + 3	
+#Decrementar Valor (-=)	x -= 3	x = x - 3	
+#Multiplicar valor (*=)	x *= 3	x = x * 3	
+#Dividir valor (/=)	x /= 3	x = x / 3	
+#modulo do valor (%=) x %= 3	x = x % 3	(RESTO DA DIVISÃO)
+#Floor division (//=)	x //= 3	x = x // 3	(RESULTADO DA DIVISÃO EXATO, SEM CASAS DECIMAIS)
+
+############
+#**=	x **= 3	x = x ** 3	
+#&=	x &= 3	x = x & 3	
+#|=	x |= 3	x = x | 3	
+#^=	x ^= 3	x = x ^ 3	
+#>>=	x >>= 3	x = x >> 3	
+#<<=	x <<= 3	x = x << 3
+
+###############################
+#Exemplos
+#Atribuir valor
+x=10
+print(x)
+
+#Incrementar Valor 
+x += 1
+print(x)
+
+#Decrementar Valor
+x -= 5
+print(x)
+
+#Multiplicar valor 
+x *= 5
+print(x)
+
+#Dividir valor (/=)	x /= 3	x = x / 3	
+#modulo do valor (%=) x %= 3	x = x % 3	(RESTO DA DIVISÃO)
+#Floor division (//=)	x //= 3	x = x // 3	(RESULTADO DA DIVISÃO EXATO, SEM CASAS DECIMAIS)
